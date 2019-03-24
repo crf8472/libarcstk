0e53c799050846f807625ecab1c1ae8e9758d3b9
tristero <crf8472@web.de>
2019-03-24 11:14:19 +0100

    Add example 'arparse'

---
+++ examples/arparse/README.md
@@ -0,0 +1,63 @@
+# Example application: arparse
+
+This example application demonstrates how libarcs is used to parse a binary
+response to an AccurateRip database request into plaintext. The response is
+provided as a file or as piped command line input.
+
+## Requirements
+
+- libarcs
+
+
+## Build
+
+Build application with just
+
+	$ make
+
+For removing all compiled and temporary files, just use
+
+	$ make clean
+
+
+## Usage
+
+### How to get the input file
+
+The ripper sends a request to AccurateRip and sometimes saves the response to a
+file. EAC for example will produce a file with a name
+starting with "dBAR" and a ".bin" suffix, like
+"dBAR-015-001b9178-014be24e-b40d2d0f.bin". EAC puts those files in
+``${USER}/Application Data/AccurateRip/AccurateRipCache``. Any of them can be
+used as input to this example.
+
+If you cannot get a response file from your ripper, you may produce a file on
+your own by actually send a query to AccurateRip. You can use ``curl`` and the
+example application [albumid](../albumid/README.md), for example try:
+
+	$ curl -s "$(../albumid/albumid /path/to/album/album.{cue,flac} | tail -1 | awk '{print $2}'))" > album_arcs.bin
+
+You can use the file ``album_arcs.bin`` as input for ``arparse``.
+
+
+### How to use the example application
+
+arparse expects the filename as parameter, just like:
+
+	$ ./arparse response_file.bin
+
+Alternatively, you can pipe the file content to arparse:
+
+	$ ./arparse < response_file.bin
+
+Or
+
+	$ cat response_file.bin | ./arparse
+
+You can just pipe the response of an actual AccurateRip query directly to
+``arparse``:
+
+	$ curl -s "$(../albumid/albumid /path/to/album/album.{cue,flac} | tail -1 | awk '{print $2}'))" | ./arparse
+
+For more information, read the comments in [arparse.cpp](./arparse.cpp).
+
---
+++ examples/arparse/arparse.cpp
@@ -0,0 +1,124 @@
+#include <cstdint>   // for uint32_t etc.
+#include <iomanip>   // for setw, setfill, hex
+#include <iostream>  // for cerr, cout
+#include <stdexcept> // for runtime_error
+#include <string>    // for string
+
+#ifndef __LIBARCS_PARSE_HPP__ // libarcs: parse AccurateRip responses
+#include <arcs/parse.hpp>
+#endif
+#ifndef __LIBARCS_IDENTIFIER_HPP__ // libarcs: parse AccurateRip responses
+#include <arcs/identifier.hpp>
+#endif
+
+#ifndef __LIBARCS_LOGGING_HPP__   // libarcs: log what you do
+#include <arcs/logging.hpp>
+#endif
+
+
+// ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+// NOTE! THIS IS EXAMPLE CODE! IT IS INTENDED TO DEMONSTRATE HOW LIBARCS COULD
+// BE USED. IT IS NOT INTENDED TO BE USED IN REAL LIFE PRODUCTION. IT IS IN NO
+// WAY TESTED FOR PRODUCTION. TAKE THIS AS A STARTING POINT TO YOUR OWN
+// SOLUTION, NOT AS A TOOL.
+// ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+
+
+int main(int argc, char* argv[])
+{
+	// Use the default parser content handler that just returns the parsed
+	// content as an object
+	std::unique_ptr<arcs::ContentHandler> content_hdlr =
+		std::make_unique<arcs::DefaultHandler>();
+	// Of course you could just write a content handler that prints every parsed
+	// entitity instead of constructing an object from it.
+
+	// Use the standard error handler that just throws an exception
+	auto error_hdlr = std::make_unique<arcs::DefaultErrorHandler>();
+
+	// The parser
+	std::unique_ptr<arcs::ARStreamParser> parser;
+
+	// The response data object
+	arcs::ARResponse response_data;
+
+	if (argc == 2) // read from the file passed
+	{
+		const std::string infilename { argv[1] };
+
+		// Create parser for files
+		parser = std::make_unique<arcs::ARFileParser>(infilename);
+	}
+	else if (argc == 1) // read from stdin
+	{
+		// Create parser for input piped via stdin
+		parser = std::make_unique<arcs::ARStdinParser>();
+	}
+	else
+	{
+		std::cout << "Arguments: " << argc << std::endl;
+		std::cout << "Usage: arparse <response_file_name>" << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	// Register parser handlers
+	parser->set_content_handler(std::move(content_hdlr));
+	parser->set_error_handler(std::move(error_hdlr));
+
+	// Run parser
+	try {
+		parser->parse();
+	} catch (const arcs::StreamReadException& e)
+	{
+		std::cerr << e.what();
+
+		return EXIT_FAILURE;
+	}
+	// ... normally you would also catch other possible exceptions, we just
+	// concentrate on libarcs.
+
+	// We positively _know_ that the ContentHandler is a DefaultHandler, so
+	// downcasting does not rise any risks. It is just not "nice". (The
+	// other method I could think of feels worse, so we stick to downcasting
+	// for now.)
+	response_data = dynamic_cast<const arcs::DefaultHandler &>
+		(parser->content_handler()).result();
+
+	std::cout << "  ARCS   Conf. Frame450" << std::endl;
+	std::cout << "-----------------------" << std::endl;
+
+	auto prev_cout_settings { std::cout.flags() }; // Save current cout settings
+
+	// Traverse the response data and print every parsed entity
+	int b {1};
+	for (const auto& block : response_data)
+	{
+		std::cout << "Block: " << b << "/" << response_data.size() << std::endl;
+
+		// Print the header of this block (which is the AccurateRip id)
+		std::cout << "ID: " << block.id().to_string() << std::endl;
+
+		for (const auto& triplet : block)
+		{
+			std::cout
+				<< std::hex << std::uppercase
+				<< std::setw(8) << std::setfill('0')
+					<< triplet.arcs()
+				<< "  "
+				<< std::dec << std::setw(2) << std::setfill(' ')
+					<< triplet.confidence()
+				<< "   "
+				<< std::hex << std::uppercase
+				<< std::setw(8) << std::setfill('0')
+					<< triplet.frame450_arcs()
+				<< std::endl;
+		}
+
+		++b;
+	}
+
+	std::cout.flags(prev_cout_settings); // restore cout settings
+
+	return EXIT_SUCCESS;
+}
+
