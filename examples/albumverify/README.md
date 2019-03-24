9eb54585c66440264b5701ecb7ec05bfb6fb6a59
tristero <crf8472@web.de>
2019-03-24 16:34:35 +0100

    Add example 'albumverify'

---
+++ examples/albumverify/README.md
@@ -0,0 +1,63 @@
+# Example application: albumverify
+
+This example application demonstrates how libarcs is used to match the checksums
+you have from your rip with the AccurateRip checksums. This example can only
+verify entire albums.
+
+
+## Requirements
+
+Dependencies for building and running:
+
+- libarcs
+
+Install ``curl`` if you want to send actual queries to AccurateRip.
+
+
+## Build
+
+Build application with just
+
+	$ make
+
+For removing all compiled and temporary files, just use
+
+	$ make clean
+
+
+## Usage
+
+Use it like:
+
+	$ ./albumverify --id=<ARId of the album> --arcs2=0x1,0x2,0x3,... <name_of_response_file>
+
+albumverify expects two mandatory parameters:
+
+- ``--id=...`` The AccurateRip id of the album in the form
+  AAA-BBBBBBBB-CCCCCCCC-DDDDDDDD
+- ``--arcs1=...`` or ``--arcs2=...`` A comma-separated list of hexadecimal
+  numbers, representing the "actual" local checksums that are to be verified.
+  You have to declare ARCSv1 values by using ``--arcs1`` and ARCSv2 values by
+  using ``--arcs2``. The values may or may not be prefixed by "0x".
+- The ``<name_of_the_response_file>`` is interpreted as a file holding the
+  binary response of an AccurateRip request. It is optional since you can also
+  pipe the binary input directly to ``albumverify``. If you are not sure how to
+  provide this input, you may consult the
+  [arparse example](../arparse/README.md).
+
+
+## Example data
+
+For convenience, ARCSv2 values of a real album along with its ARId are provided
+as test input. The correct values reside in ``example_sums_ok.txt`` while a
+version with deviating values for tracks 6, 12 and 15 resides in
+``example_sums_fail.txt``. The ARId along with the specific Query URL resides in
+``example_arid.txt``.
+
+On Linux these input data can be verified against the values from AccurateRip as
+follows:
+
+	$ curl -s "$(tail -1 < example_arid.txt)" | ./albumverify --id=$(head -1 < example_arid.txt) --arcs2="$(xargs < example_sums_ok.txt | tr ' ' ',')"
+
+For more information, read the comments in [albumverify.cpp](./albumverify.cpp).
+
---
+++ examples/albumverify/albumverify.cpp
@@ -0,0 +1,282 @@
+#include <algorithm> // for count
+#include <cstdint>   // for uint32_t etc.
+#include <cstring>   // for strtok
+#include <iomanip>   // for setw, setfill, hex
+#include <iostream>  // for cerr, cout
+#include <stdexcept> // for runtime_error
+#include <string>    // for string
+
+#ifndef __LIBARCS_MATCH_HPP__ // libarcs: match Checksums and ARResponse
+#include <arcs/match.hpp>
+#endif
+#ifndef __LIBARCS_CALCULATE_HPP__
+#include <arcs/calculate.hpp> // for arcs::Checksums
+#endif
+#ifndef __LIBARCS_PARSE_HPP__
+#include <arcs/parse.hpp> // for arcs::ARResponse
+#endif
+
+#ifndef __LIBARCS_LOGGING_HPP__   // libarcs: log what you do
+#include <arcs/logging.hpp>
+#endif
+
+
+// ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+// NOTE! THIS IS EXAMPLE CODE! IT IS INTENDED TO DEMONSTRATE HOW LIBARCS COULD
+// BE USED. IT IS NOT INTENDED TO BE USED IN REAL LIFE PRODUCTION. IT IS IN NO
+// WAY TESTED FOR PRODUCTION. TAKE THIS AS A STARTING POINT TO YOUR OWN
+// SOLUTION, NOT AS A TOOL.
+// ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+
+
+// NOTE 2: This example is rather long, much longer than I would prefer it to
+// be. The reason for this longishness is that for using the match interface,
+// you have to prepare a lot of input:
+//
+// - your own sums, i.e. the sums you have from your rip
+// - the reference sums from AccurateRip and
+// - the AccurateRip id of the album.
+//
+// Providing all this input in a way acceptable in example code, some
+// boilerplate code is inevitably. Although the boilerplate does not carry any
+// didactic evidence for libarcs, I wanted to provide an example that you can
+// feed your own input in.
+// In fact, the functions parse_arid(), parse_input_arcs() and
+// parse_match_arcs() are more or less quick and dirty boilerplate code for just
+// providing the required input values. In real life code, you would have to
+// invest (much) more effort to make the code robust but this is not related to
+// the libarcs API.
+// The actual example demonstrating the use of the ListMatcher class is
+// contained in main(). It's very simple to use. Have fun!
+
+
+/**
+ * Parse the ARId from the command line
+ *
+ * \param[in] input_id A string representation of the ARId
+ *
+ * \return Parsed ARId
+ */
+arcs::ARId parse_arid(const char* input_id)
+{
+	const std::string id_str { input_id };
+
+	uint16_t track_count = std::stoul(id_str.substr(0, 3), nullptr, 10);
+	uint32_t id_1 = std::stoul(id_str.substr(4, 8), nullptr, 16);
+	uint32_t id_2 = std::stoul(id_str.substr(13, 8), nullptr, 16);
+	uint32_t cddb_id = std::stoul(id_str.substr(22, 8), nullptr, 16);
+
+	return arcs::ARId(track_count, id_1, id_2, cddb_id);
+}
+
+
+/**
+ * Parse a comma-separated list of hexadecimal numbers
+ *
+ * \param[in] list A string representation of the list
+ * \param[in] t The checksum type to declare (either ARCS1 or ARCS2)
+ *
+ * \return Parsed Checksums
+ */
+arcs::Checksums parse_input_arcs(const char* list, const arcs::checksum::type t)
+{
+	const std::string checksum_list { list };
+
+	const size_t total_tracks =
+		1 + std::count(checksum_list.begin(), checksum_list.end(), ',');
+
+	arcs::Checksums checksums { total_tracks };
+
+	std::string token;
+	uint32_t arcs;
+
+	std::string::size_type token_start = 0;
+	std::string::size_type token_end   = checksum_list.find_first_of(',');
+
+	auto prev_settings = std::cout.flags();
+	std::cout << "My checksums to match:" << std::endl;
+	for (std::size_t i = 0; i < total_tracks; ++i)
+	{
+		token = checksum_list.substr(token_start, token_end - token_start);
+		arcs = std::stoul(token, nullptr, 16);
+
+		std::cout << "Track "
+			<< std::dec
+			<< std::setw(2) << std::setfill(' ') << (i + 1)
+			<< ": "
+			<< std::hex << std::uppercase
+			<< std::setw(8) << std::setfill('0')
+			<< arcs
+			<< std::dec << "  (chars: "
+			<< std::setw(3) << std::setfill(' ') << token_start
+			<< " - " << std::setw(3) << std::setfill(' ') << token_end << ")"
+			<< '\n';
+
+		checksums[i].insert(t, arcs::Checksum(std::stoul(token, nullptr, 16)));
+		token_start = token_end + 1;
+		token_end = checksum_list.find_first_of(',', token_start);
+
+		if (token_end == std::string::npos)
+		{
+			token_end = checksum_list.length();
+		}
+	}
+	std::cout.flags(prev_settings);
+
+	return checksums;
+}
+
+
+/**
+ * Parse ARCSs from a non-empty response file or from stdin
+ *
+ * \param[in] filename Name of the response file (maybe empty, then stdin)
+ *
+ * \return Parsed ARResponse
+ */
+arcs::ARResponse parse_match_arcs(const std::string &filename)
+{
+	std::unique_ptr<arcs::ContentHandler> content_hdlr =
+		std::make_unique<arcs::DefaultHandler>();
+
+	auto error_hdlr = std::make_unique<arcs::DefaultErrorHandler>();
+
+	std::unique_ptr<arcs::ARStreamParser> parser;
+
+	if (filename.empty())
+	{
+		parser = std::make_unique<arcs::ARStdinParser>();
+	} else
+	{
+		parser = std::make_unique<arcs::ARFileParser>(filename);
+	}
+
+	// Register parser handlers
+	parser->set_content_handler(std::move(content_hdlr));
+	parser->set_error_handler(std::move(error_hdlr));
+
+	// Run parser (this may throw!)
+	parser->parse();
+
+	// We positively _know_ that the ContentHandler is a DefaultHandler, so
+	// downcasting does not rise any risks. It is just not "nice". (The
+	// other method I could think of feels worse, so we stick to downcasting
+	// for now.)
+	return dynamic_cast<const arcs::DefaultHandler &>
+		(parser->content_handler()).result();
+}
+
+
+int main(int argc, char* argv[])
+{
+	// Do only the absolutely inevitable checking
+	if (argc < 3 or argc > 4)
+	{
+		std::cout <<
+			"Usage: albumverify --id=<ARId> --arcs2=0xA,0xB,0xC,... <file.bin>"
+			<< std::endl;
+
+		return EXIT_SUCCESS;
+	}
+
+	// If you like, you can activate the internal logging of libarcs to
+	// see what's going on behind the scenes. We provide an appender for stdout
+	// and set the loglevel to 'INFO', which means you should probably not see
+	// anything unless you give libarcs unexpected input.
+	arcs::Logging::instance().add_appender(
+			std::make_unique<arcs::Appender>("stdout", stdout));
+
+	// Set this to LOG_DEBUG or LOG_DEBUG1 if you want to see what libarcs is
+	// doing with your input
+	arcs::Logging::instance().set_level(arcs::LOG_INFO);
+
+	// Parse the AccurateRip id of the album passed from the command line
+	const arcs::ARId arid = parse_arid( argv[1] + 5 );
+
+	std::cout << "Album ID: " << arid.to_string() << '\n';
+
+	// Parse declared ARCS type (ARCSv1 or ARCSv2)
+	arcs::checksum::type type = argv[2][6] == '1'
+		? arcs::checksum::type::ARCS1
+		: arcs::checksum::type::ARCS2;
+
+	// Parse the checksums of the album passed from the command line
+	const arcs::Checksums checksums = parse_input_arcs( argv[2] + 8, type );
+
+	// Parse the checksums to be matched from file or stdin
+	std::string filename;
+	if (argc == 4)
+	{
+		filename = std::string(argv[3]);
+	}
+	const arcs::ARResponse arcss = parse_match_arcs(filename);
+
+	// Now the interesting part: peform the match.
+	// The ListMatcher class targets situations in which you have a list of
+	// checksums and you _know_ in which order they form the album. Therefore
+	// ListMatcher is the device of choice here.
+	arcs::ListMatcher matcher(checksums, arid, arcss);
+	// It may also be the case that you have just some tracks of an album or you
+	// cannot be sure about the order. In this case, you would use the
+	// arcs::AnyMatcher.
+
+	// Inform about the result
+	std::cout << "RESULT: ";
+	if (matcher.matches())
+	{
+		std::cout << "Response contains a total match in block "
+			<< matcher.best_match()
+			<< ", which is of type ARCSv" << (matcher.matches_v2() + 1)
+			<< "." << std::endl;
+	} else
+	{
+		std::cout << "No total match. Best block is "
+			<< matcher.best_match()
+			<< ", which is of type ARCSv" << (matcher.matches_v2() + 1)
+			<< " with difference " << matcher.best_difference()
+			<< std::endl;
+	}
+
+	// And now print the gory details
+
+	auto match { matcher.match() };
+	auto block { matcher.best_match() };
+	int trackno {0};
+	bool is_match = false;
+
+	auto prev_settings = std::cout.flags();
+
+	std::cout << "TRACK   MINE      THEIRS\n";
+
+	for (const auto& track : arcss[block])
+	{
+		// The match object stores flags for every check that the matcher
+		// has performed. Thus, the result of the matching can be queried on the
+		// match object by just giving the coordinate block/track/version.
+		is_match =
+			match->track(block, trackno, type == arcs::checksum::type::ARCS2);
+
+		std::cout << " " << std::dec << std::setw(2) << std::setfill('0')
+			<< (trackno + 1) << ":  ";
+
+		std::cout << std::hex << std::uppercase;
+
+		std::cout << std::setw(8) << std::setfill('0')
+			<< checksums[trackno].get(type).value();
+
+		std::cout << (is_match ? " = " : "   ") ;
+
+		std::cout << std::setw(8) << std::setfill('0') << track.arcs();
+
+		std::cout << (is_match ? " [OK]" : " <- FAIL") ;
+
+		std::cout << '\n';
+
+		++trackno;
+	}
+
+	std::cout.flags(prev_settings);
+
+	return EXIT_SUCCESS;
+}
+
