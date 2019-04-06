74c6dcccc7734bf5e19e46dc1ca60477bf876987
tristero <crf8472@web.de>
2019-04-06 17:43:35 +0200

    Bugfix: remove unnecessary recompile of tests

--- test/src/main.test.cpp
+++ test/src/main.test.cpp
@@ -3,8 +3,5 @@
 
 /**
  * \file main.test.cpp The main file for the Catch2 tester binary
- *
- * This file is linked with all compiled tests in this directory. It can be run
- * by ctest after building the project (with -DWITH_TESTS=ON).
  */
 
