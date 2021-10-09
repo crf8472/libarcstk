b13086c1a722fc81d97dbf36aa5335597dfe60fd
tristero <crf8472@web.de>
2021-10-09 20:36:44 +0200

    Add test for version.cpp

---
+++ test/src/version.cpp
@@ -0,0 +1,40 @@
+#include "catch2/catch.hpp"
+
+
+#ifndef __LIBARCSTK_VERSION_HPP__
+#include "version.hpp"
+#endif
+
+/**
+ * \file Fixtures for functions in version.cpp
+ */
+
+
+TEST_CASE ( "constants", "[version]" )
+{
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_NAME == "libarcstk" );
+
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_MAJOR  == 0 );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_MINOR  == 1 );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_PATCH  == 0 );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_SUFFIX == "rc.2" );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION        == "0.1.0-rc.2" );
+}
+
+
+TEST_CASE ( "functions", "[version]" )
+{
+	using arcstk::v_1_0_0::api_version_is_at_least;
+
+	SECTION ("api_version_is_at_least")
+	{
+		CHECK (  api_version_is_at_least(0, 0, 0) );
+		CHECK (  api_version_is_at_least(0, 0, 1) );
+		CHECK (  api_version_is_at_least(0, 1, 0) );
+		CHECK ( !api_version_is_at_least(0, 1, 1) );
+		CHECK ( !api_version_is_at_least(1, 0, 0) );
+		CHECK ( !api_version_is_at_least(1, 1, 0) );
+		CHECK ( !api_version_is_at_least(1, 1, 1) );
+	}
+}
+
