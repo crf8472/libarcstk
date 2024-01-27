53e62a695c7e4a69ce486011ecd5b3c593897b38
tristero <crf8472@web.de>
2024-01-27 11:17:25 +0100

    Adjust TracksetVerifier tests

--- test/src/verify.cpp
+++ test/src/verify.cpp
@@ -2328,7 +2328,7 @@ TEST_CASE ( "TracksetVerifier", "[tracksetverifier]" )
 	track15.insert(type::ARCS2, Checksum(0x58FC3C3E));
 	track15.insert(type::ARCS1, Checksum(0x5FE8B032));
 
-	Checksums result1 {
+	Checksums actual_sums {
 		track01,
 		track02,
 		track03,
@@ -2346,15 +2346,36 @@ TEST_CASE ( "TracksetVerifier", "[tracksetverifier]" )
 		track15
 	};
 
-	REQUIRE ( result1.size() == 15 );
+	REQUIRE ( actual_sums.size() == 15 );
 
 
 	// Construct ARResponse by hand
 
 	ARId id { 15, 0x001B9178, 0x014BE24E, 0xB40D2D0F };
 
-	// Define block: v1 values
+	// Define block: mismatching values
 	ARBlock block0( /* id */ { 15, 0x001B9178, 0x014BE24E, 0xB40D2D0F },
+		{ /* triplets */
+			{ 0xC89192E5, 0, 0 },
+			{ 0x4F78EB03, 0, 0 },
+			{ 0x56582281, 0, 0 },
+			{ 0x0E2187F9, 0, 0 },
+			{ 0x2BE71E50, 0, 0 },
+			{ 0x01E7235D, 0, 0 },
+			{ 0xD8F6763C, 0, 0 },
+			{ 0x8480331E, 0, 0 },
+			{ 0x42F5061C, 0, 0 },
+			{ 0x47D70F02, 0, 0 },
+			{ 0xBABF08AA, 0, 0 },
+			{ 0x563EFECB, 0, 0 },
+			{ 0xAB123C9C, 0, 0 },
+			{ 0xB65C20E4, 0, 0 },
+			{ 0x68FC3C3E, 0, 0 }
+		}
+	);
+
+	// Define block: v1 values
+	ARBlock block1( /* id */ { 15, 0x001B9178, 0x014BE24E, 0xB40D2D0F },
 		{ /* triplets */
 			{ 0x98B10E0F, 24, 0 },
 			{ 0x475F57E9, 24, 0 },
@@ -2374,45 +2395,24 @@ TEST_CASE ( "TracksetVerifier", "[tracksetverifier]" )
 		}
 	);
 
-	// Define block: v2 values, but different id
-	ARBlock block1( /* id */ { 1523, 0x001B9178, 0x014BE24E, 0xB40D2D0F },
-		{ /* triplets */
-			{ 0xB89992E5, 24, 0 },
-			{ 0x4F77EB03, 24, 0 },
-			{ 0x56582282, 24, 0 },
-			{ 0x9E2187F9, 24, 0 },
-			{ 0x6BE71E50, 24, 0 },
-			{ 0x01E7235F, 24, 0 },
-			{ 0xD8F7763C, 24, 0 },
-			{ 0x8480223E, 24, 0 },
-			{ 0x42C5061C, 24, 0 },
-			{ 0x47A70F02, 24, 0 },
-			{ 0xBABF08CC, 24, 0 },
-			{ 0x563EDCCB, 24, 0 },
-			{ 0xAB123C7C, 24, 0 },
-			{ 0xC65C20E4, 24, 0 },
-			{ 0x58FC3C3E, 24, 0 }
-		}
-	);
-
-	// Define block: v2 values same as block 2, id from block 1
+	// Define block: v2 values (best block is last on traversal)
 	ARBlock block2( /* id */ { 15, 0x001B9178, 0x014BE24E, 0xB40D2D0F },
 		{ /* triplets */
-			{ 0xB89992E5, 24, 0 },
-			{ 0x4F77EB03, 24, 0 },
-			{ 0x56582282, 24, 0 },
-			{ 0x9E2187F9, 24, 0 },
-			{ 0x6BE71E50, 24, 0 },
-			{ 0x01E7235F, 24, 0 },
-			{ 0xD8F7763C, 24, 0 },
-			{ 0x8480223E, 24, 0 },
-			{ 0x42C5061C, 24, 0 },
-			{ 0x47A70F02, 24, 0 },
-			{ 0xBABF08CC, 24, 0 },
-			{ 0x563EDCCB, 24, 0 },
-			{ 0xAB123C7C, 24, 0 },
-			{ 0xC65C20E4, 24, 0 },
-			{ 0x58FC3C3E, 24, 0 }
+			{ 0xB89992E5,  6, 0 },
+			{ 0x4F77EB03,  8, 0 },
+			{ 0x56582282,  7, 0 },
+			{ 0x9E2187F9,  9, 0 },
+			{ 0x6BE71E50,  2, 0 },
+			{ 0x01E7235F,  1, 0 },
+			{ 0xD8F7763C,  0, 0 },
+			{ 0x8480223E, 13, 0 },
+			{ 0x42C5061C, 16, 0 },
+			{ 0x47A70F02, 17, 0 },
+			{ 0xBABF08CC, 18, 0 },
+			{ 0x563EDCCB, 21, 0 },
+			{ 0xAB123C7C, 14, 0 },
+			{ 0xC65C20E4, 26, 0 },
+			{ 0x58FC3C3E, 28, 0 }
 		}
 	);
 
@@ -2441,43 +2441,43 @@ TEST_CASE ( "TracksetVerifier", "[tracksetverifier]" )
 	//o = std::make_unique<arcstk::details::UnknownOrder>();
 	//const auto sov_result = arcstk::verify(result1, arcstk::EmptyARId,
 	//		arcstk::FromResponse(&response), *t, *o);
-	arcstk::TracksetVerifier t(result1);
-	const auto sov_result = t.perform(response);
+	arcstk::TracksetVerifier t(actual_sums);
+	const auto result = t.perform(response);
 
-	const auto sov_best_block = sov_result->best_block();
+	const auto sov_best_block = result->best_block();
 
 	SECTION ( "Non-Strict TrackOrder Verification is successful" )
 	{
-		//CHECK ( sov_result->all_tracks_verified() );
+		CHECK ( result->all_tracks_verified() );
 
-		CHECK ( sov_result->is_verified(0) );
-		CHECK ( sov_result->is_verified(1) );
-		CHECK ( sov_result->is_verified(2) );
-		CHECK ( sov_result->is_verified(3) );
-		CHECK ( sov_result->is_verified(4) );
-		CHECK ( sov_result->is_verified(5) );
-		CHECK ( sov_result->is_verified(6) );
-		CHECK ( sov_result->is_verified(7) );
-		CHECK ( sov_result->is_verified(8) );
-		CHECK ( sov_result->is_verified(9) );
-		CHECK ( sov_result->is_verified(10) );
-		CHECK ( sov_result->is_verified(11) );
-		CHECK ( sov_result->is_verified(12) );
-		CHECK ( sov_result->is_verified(13) );
-		CHECK ( sov_result->is_verified(14) );
+		CHECK ( result->is_verified(0) );
+		CHECK ( result->is_verified(1) );
+		CHECK ( result->is_verified(2) );
+		CHECK ( result->is_verified(3) );
+		CHECK ( result->is_verified(4) );
+		CHECK ( result->is_verified(5) );
+		CHECK ( result->is_verified(6) );
+		CHECK ( result->is_verified(7) );
+		CHECK ( result->is_verified(8) );
+		CHECK ( result->is_verified(9) );
+		CHECK ( result->is_verified(10) );
+		CHECK ( result->is_verified(11) );
+		CHECK ( result->is_verified(12) );
+		CHECK ( result->is_verified(13) );
+		CHECK ( result->is_verified(14) );
 	}
 
 	SECTION ( "Non-Strict TrackOrder Verification result fails on accessing"
 			" illegal track" )
 	{
-		CHECK_THROWS ( sov_result->is_verified(15) );
+		CHECK_THROWS ( result->is_verified(15) );
 	}
 
 	SECTION ( "Non-Strict TrackOrder Verification result has correct size" )
 	{
-		CHECK ( sov_result->total_blocks() == 3 );
-		CHECK ( sov_result->tracks_per_block() == 15 );
-		CHECK ( sov_result->size() == 93 ); // 2 * blocks * tracks + blocks
+		CHECK ( result->total_blocks() == 3 );
+		CHECK ( result->tracks_per_block() == 15 );
+		CHECK ( result->size() == 93 ); // 2 * blocks * tracks + blocks
 	}
 
 	SECTION ( "Non-Strict TrackOrder Verification finds best block" )
@@ -2487,132 +2487,132 @@ TEST_CASE ( "TracksetVerifier", "[tracksetverifier]" )
 		CHECK ( std::get<1>(sov_best_block) == v2 );
 
 		CHECK ( std::get<2>(sov_best_block) == 0 );
-		CHECK ( sov_result->best_block_difference() == 0 );
+		CHECK ( result->best_block_difference() == 0 );
 	}
 
 	SECTION ( "Non-Strict TrackOrder Verification result has correct flags" )
 	{
 		// 0
-		CHECK ( sov_result->id(0) );
+		CHECK ( result->id(0) ); // only mismatches
 
-		CHECK ( sov_result->track(0,  0, v1) );
-		CHECK ( sov_result->track(0,  1, v1) );
-		CHECK ( sov_result->track(0,  2, v1) );
-		CHECK ( sov_result->track(0,  3, v1) );
-		CHECK ( sov_result->track(0,  4, v1) );
-		CHECK ( sov_result->track(0,  5, v1) );
-		CHECK ( sov_result->track(0,  6, v1) );
-		CHECK ( sov_result->track(0,  7, v1) );
-		CHECK ( sov_result->track(0,  8, v1) );
-		CHECK ( sov_result->track(0,  9, v1) );
-		CHECK ( sov_result->track(0, 10, v1) );
-		CHECK ( sov_result->track(0, 11, v1) );
-		CHECK ( sov_result->track(0, 12, v1) );
-		CHECK ( sov_result->track(0, 13, v1) );
-		CHECK ( sov_result->track(0, 14, v1) );
+		CHECK ( not result->track(0,  0, v1) );
+		CHECK ( not result->track(0,  1, v1) );
+		CHECK ( not result->track(0,  2, v1) );
+		CHECK ( not result->track(0,  3, v1) );
+		CHECK ( not result->track(0,  4, v1) );
+		CHECK ( not result->track(0,  5, v1) );
+		CHECK ( not result->track(0,  6, v1) );
+		CHECK ( not result->track(0,  7, v1) );
+		CHECK ( not result->track(0,  8, v1) );
+		CHECK ( not result->track(0,  9, v1) );
+		CHECK ( not result->track(0, 10, v1) );
+		CHECK ( not result->track(0, 11, v1) );
+		CHECK ( not result->track(0, 12, v1) );
+		CHECK ( not result->track(0, 13, v1) );
+		CHECK ( not result->track(0, 14, v1) );
 
-		CHECK ( not sov_result->track(0,  0, v2) );
-		CHECK ( not sov_result->track(0,  1, v2) );
-		CHECK ( not sov_result->track(0,  2, v2) );
-		CHECK ( not sov_result->track(0,  3, v2) );
-		CHECK ( not sov_result->track(0,  4, v2) );
-		CHECK ( not sov_result->track(0,  5, v2) );
-		CHECK ( not sov_result->track(0,  6, v2) );
-		CHECK ( not sov_result->track(0,  7, v2) );
-		CHECK ( not sov_result->track(0,  8, v2) );
-		CHECK ( not sov_result->track(0,  9, v2) );
-		CHECK ( not sov_result->track(0, 10, v2) );
-		CHECK ( not sov_result->track(0, 11, v2) );
-		CHECK ( not sov_result->track(0, 12, v2) );
-		CHECK ( not sov_result->track(0, 13, v2) );
-		CHECK ( not sov_result->track(0, 14, v2) );
+		CHECK ( not result->track(0,  0, v2) );
+		CHECK ( not result->track(0,  1, v2) );
+		CHECK ( not result->track(0,  2, v2) );
+		CHECK ( not result->track(0,  3, v2) );
+		CHECK ( not result->track(0,  4, v2) );
+		CHECK ( not result->track(0,  5, v2) );
+		CHECK ( not result->track(0,  6, v2) );
+		CHECK ( not result->track(0,  7, v2) );
+		CHECK ( not result->track(0,  8, v2) );
+		CHECK ( not result->track(0,  9, v2) );
+		CHECK ( not result->track(0, 10, v2) );
+		CHECK ( not result->track(0, 11, v2) );
+		CHECK ( not result->track(0, 12, v2) );
+		CHECK ( not result->track(0, 13, v2) );
+		CHECK ( not result->track(0, 14, v2) );
 
 		// 1
-		CHECK ( sov_result->id(1) ); // differs from stv_result: any id is verified
+		CHECK ( result->id(1) ); // all v1 match
 
-		CHECK ( not sov_result->track(1,  0, v1) );
-		CHECK ( not sov_result->track(1,  1, v1) );
-		CHECK ( not sov_result->track(1,  2, v1) );
-		CHECK ( not sov_result->track(1,  3, v1) );
-		CHECK ( not sov_result->track(1,  4, v1) );
-		CHECK ( not sov_result->track(1,  5, v1) );
-		CHECK ( not sov_result->track(1,  6, v1) );
-		CHECK ( not sov_result->track(1,  7, v1) );
-		CHECK ( not sov_result->track(1,  8, v1) );
-		CHECK ( not sov_result->track(1,  9, v1) );
-		CHECK ( not sov_result->track(1, 10, v1) );
-		CHECK ( not sov_result->track(1, 11, v1) );
-		CHECK ( not sov_result->track(1, 12, v1) );
-		CHECK ( not sov_result->track(1, 13, v1) );
-		CHECK ( not sov_result->track(1, 14, v1) );
+		CHECK ( result->track(1,  0, v1) );
+		CHECK ( result->track(1,  1, v1) );
+		CHECK ( result->track(1,  2, v1) );
+		CHECK ( result->track(1,  3, v1) );
+		CHECK ( result->track(1,  4, v1) );
+		CHECK ( result->track(1,  5, v1) );
+		CHECK ( result->track(1,  6, v1) );
+		CHECK ( result->track(1,  7, v1) );
+		CHECK ( result->track(1,  8, v1) );
+		CHECK ( result->track(1,  9, v1) );
+		CHECK ( result->track(1, 10, v1) );
+		CHECK ( result->track(1, 11, v1) );
+		CHECK ( result->track(1, 12, v1) );
+		CHECK ( result->track(1, 13, v1) );
+		CHECK ( result->track(1, 14, v1) );
 
-		CHECK ( sov_result->track(1,  0, v2) );
-		CHECK ( sov_result->track(1,  1, v2) );
-		CHECK ( sov_result->track(1,  2, v2) );
-		CHECK ( sov_result->track(1,  3, v2) );
-		CHECK ( sov_result->track(1,  4, v2) );
-		CHECK ( sov_result->track(1,  5, v2) );
-		CHECK ( sov_result->track(1,  6, v2) );
-		CHECK ( sov_result->track(1,  7, v2) );
-		CHECK ( sov_result->track(1,  8, v2) );
-		CHECK ( sov_result->track(1,  9, v2) );
-		CHECK ( sov_result->track(1, 10, v2) );
-		CHECK ( sov_result->track(1, 11, v2) );
-		CHECK ( sov_result->track(1, 12, v2) );
-		CHECK ( sov_result->track(1, 13, v2) );
-		CHECK ( sov_result->track(1, 14, v2) );
+		CHECK ( not result->track(1,  0, v2) );
+		CHECK ( not result->track(1,  1, v2) );
+		CHECK ( not result->track(1,  2, v2) );
+		CHECK ( not result->track(1,  3, v2) );
+		CHECK ( not result->track(1,  4, v2) );
+		CHECK ( not result->track(1,  5, v2) );
+		CHECK ( not result->track(1,  6, v2) );
+		CHECK ( not result->track(1,  7, v2) );
+		CHECK ( not result->track(1,  8, v2) );
+		CHECK ( not result->track(1,  9, v2) );
+		CHECK ( not result->track(1, 10, v2) );
+		CHECK ( not result->track(1, 11, v2) );
+		CHECK ( not result->track(1, 12, v2) );
+		CHECK ( not result->track(1, 13, v2) );
+		CHECK ( not result->track(1, 14, v2) );
 
 		// 2
-		CHECK ( sov_result->id(2) );
+		CHECK ( result->id(2) ); // all v2 match
 
-		CHECK ( not sov_result->track(2,  0, v1) );
-		CHECK ( not sov_result->track(2,  1, v1) );
-		CHECK ( not sov_result->track(2,  2, v1) );
-		CHECK ( not sov_result->track(2,  3, v1) );
-		CHECK ( not sov_result->track(2,  4, v1) );
-		CHECK ( not sov_result->track(2,  5, v1) );
-		CHECK ( not sov_result->track(2,  6, v1) );
-		CHECK ( not sov_result->track(2,  7, v1) );
-		CHECK ( not sov_result->track(2,  8, v1) );
-		CHECK ( not sov_result->track(2,  9, v1) );
-		CHECK ( not sov_result->track(2, 10, v1) );
-		CHECK ( not sov_result->track(2, 11, v1) );
-		CHECK ( not sov_result->track(2, 12, v1) );
-		CHECK ( not sov_result->track(2, 13, v1) );
-		CHECK ( not sov_result->track(2, 14, v1) );
+		CHECK ( not result->track(2,  0, v1) );
+		CHECK ( not result->track(2,  1, v1) );
+		CHECK ( not result->track(2,  2, v1) );
+		CHECK ( not result->track(2,  3, v1) );
+		CHECK ( not result->track(2,  4, v1) );
+		CHECK ( not result->track(2,  5, v1) );
+		CHECK ( not result->track(2,  6, v1) );
+		CHECK ( not result->track(2,  7, v1) );
+		CHECK ( not result->track(2,  8, v1) );
+		CHECK ( not result->track(2,  9, v1) );
+		CHECK ( not result->track(2, 10, v1) );
+		CHECK ( not result->track(2, 11, v1) );
+		CHECK ( not result->track(2, 12, v1) );
+		CHECK ( not result->track(2, 13, v1) );
+		CHECK ( not result->track(2, 14, v1) );
 
-		CHECK ( sov_result->track(2,  0, v2) );
-		CHECK ( sov_result->track(2,  1, v2) );
-		CHECK ( sov_result->track(2,  2, v2) );
-		CHECK ( sov_result->track(2,  3, v2) );
-		CHECK ( sov_result->track(2,  4, v2) );
-		CHECK ( sov_result->track(2,  5, v2) );
-		CHECK ( sov_result->track(2,  6, v2) );
-		CHECK ( sov_result->track(2,  7, v2) );
-		CHECK ( sov_result->track(2,  8, v2) );
-		CHECK ( sov_result->track(2,  9, v2) );
-		CHECK ( sov_result->track(2, 10, v2) );
-		CHECK ( sov_result->track(2, 11, v2) );
-		CHECK ( sov_result->track(2, 12, v2) );
-		CHECK ( sov_result->track(2, 13, v2) );
-		CHECK ( sov_result->track(2, 14, v2) );
+		CHECK ( result->track(2,  0, v2) );
+		CHECK ( result->track(2,  1, v2) );
+		CHECK ( result->track(2,  2, v2) );
+		CHECK ( result->track(2,  3, v2) );
+		CHECK ( result->track(2,  4, v2) );
+		CHECK ( result->track(2,  5, v2) );
+		CHECK ( result->track(2,  6, v2) );
+		CHECK ( result->track(2,  7, v2) );
+		CHECK ( result->track(2,  8, v2) );
+		CHECK ( result->track(2,  9, v2) );
+		CHECK ( result->track(2, 10, v2) );
+		CHECK ( result->track(2, 11, v2) );
+		CHECK ( result->track(2, 12, v2) );
+		CHECK ( result->track(2, 13, v2) );
+		CHECK ( result->track(2, 14, v2) );
 
 
-		CHECK_THROWS ( sov_result->id(3) );            // illegal block
-		CHECK_THROWS ( sov_result->track(3, 14, v2) ); //         block
-		CHECK_THROWS ( sov_result->track(2, 15, v2) ); //         track
+		CHECK_THROWS ( result->id(3) );            // illegal block
+		CHECK_THROWS ( result->track(3, 14, v2) ); //         block
+		CHECK_THROWS ( result->track(2, 15, v2) ); //         track
 
-		CHECK ( sov_result->difference(0, v1) ==  0 );
-		CHECK ( sov_result->difference(0, v2) == 15 );
+		CHECK ( result->difference(0, v1) == 15 );
+		CHECK ( result->difference(0, v2) == 15 );
 
-		CHECK ( sov_result->difference(1, v1) == 15 ); // differs from stv_result
-		CHECK ( sov_result->difference(1, v2) ==  0 ); // differs from stv_result
+		CHECK ( result->difference(1, v1) ==  0 );
+		CHECK ( result->difference(1, v2) == 15 );
 
-		CHECK ( sov_result->difference(2, v1) == 15 );
-		CHECK ( sov_result->difference(2, v2) ==  0 );
+		CHECK ( result->difference(2, v1) == 15 );
+		CHECK ( result->difference(2, v2) ==  0 );
 
-		CHECK_THROWS ( sov_result->difference(3, v1) == 0 );
-		CHECK_THROWS ( sov_result->difference(3, v2) == 0 );
+		CHECK_THROWS ( result->difference(3, v1) == 0 );
+		CHECK_THROWS ( result->difference(3, v2) == 0 );
 	}
 }
 
