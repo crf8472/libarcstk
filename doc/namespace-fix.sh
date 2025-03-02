203e11219e69612e57c4aa41d250dce98e8f7f7e
tristero <crf8472@mailbox.org>
2025-03-03 00:55:55 +0100

    Fix

--- doc/namespace-fix.sh
+++ doc/namespace-fix.sh
@@ -3,7 +3,7 @@
 ## Insert inline namespace name to all classes where 1.13.2 doxygen ignored it.
 
 ## Names of the classes known to be broken
-FNAME=( 'Verifier' 'VerificationResult' )
+CLASSES=( 'Verifier' 'VerificationResult' )
 NS='arcstk'
 
 PREFIX="generated-docs/mcss/xml/class${NS}_1_1v__1__0__0_1_1"
