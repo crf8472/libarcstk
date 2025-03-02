e2a28813a5346295faebe1132c95cb7cdbdba781
tristero <crf8472@mailbox.org>
2025-03-03 00:34:37 +0100

    Add fix for missing inline namespaces

---
+++ doc/namespace-fix.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+##
+## Insert inline namespace name to all classes where 1.13.2 doxygen ignored it.
+
+## Names of the classes known to be broken
+FNAME=( 'Verifier' 'VerificationResult' )
+NS='arcstk'
+
+PREFIX="generated-docs/mcss/xml/class${NS}_1_1v__1__0__0_1_1"
+
+for CLASS in ${CLASSES[@]}; do
+	FILE="${PREFIX}${CLASS}.xml"
+	printf "Patch %s\n" "${FILE}"
+	sed -i "s/\(${NS}\)::\(${CLASS}\)/\1::v_1_0_0::\2/g" "${FILE}"
+done
+
