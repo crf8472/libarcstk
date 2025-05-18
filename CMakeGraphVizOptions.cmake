e15f015b67f1382078f0cba6b06ad0330b4984c1
tristero <crf8472@mailbox.org>
2025-05-18 14:17:31 +0200

    Update CMakeGraphVizOptions.cmake

--- CMakeGraphVizOptions.cmake
+++ CMakeGraphVizOptions.cmake
@@ -7,16 +7,19 @@
 ##
 ## Debug-/Development only. Do not use in production builds!
 
-## Activate dot-graph for target dependencies
-
-set (GRAPHVIZ_CUSTOM_TARGETS ON )
-
-## Leave defaults untouched
+## Uncomment to toggle default value
 
 #set (GRAPHVIZ_EXECUTABLES    OFF )
 #set (GRAPHVIZ_STATIC_LIBS    OFF )
+#set (GRAPHVIZ_SHARED_LIBS    OFF )
 #set (GRAPHVIZ_MODULE_LIBS    OFF )
-#set (GRAPHVIZ_OBJECT_LIBS    OFF )
 #set (GRAPHVIZ_INTERFACE_LIBS OFF )
+#set (GRAPHVIZ_OBJECT_LIBS    OFF )
 #set (GRAPHVIZ_UNKNOWN_LIBS   OFF )
+#set (GRAPHVIZ_EXTERNAL_LIBS  OFF )
+set (GRAPHVIZ_CUSTOM_TARGETS  ON  ) # Activate dot-graph for target dependencies
+#set (GRAPHVIZ_IGNORE_TARGETS  libarcstk )
+
+#set (GRAPHVIZ_GENERATE_PER_TARGET OFF )
+#set (GRAPHVIZ_GENERATE_DEPENDERS  OFF )
 
