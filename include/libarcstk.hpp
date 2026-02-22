9a81480f1a301498bdcdf94494b98878610ae863
tristero <crf8472@mailbox.org>
2026-02-22 14:57:54 +0100

    Add include-all header

---
+++ include/libarcstk.hpp
@@ -0,0 +1,93 @@
+#ifndef __LIBARCSTK_LIBARCSTK_HPP__
+#define __LIBARCSTK_LIBARCSTK_HPP__
+
+
+/**
+ * \file
+ *
+ * \brief libarcstk API.
+ *
+ * \details
+ *
+ * Include the entire API of libarcstk.
+ */
+
+
+/* Logging macros */
+
+#ifndef __LIBARCSTK_LOGGING_HPP__
+#include <arcstk/logging.hpp>
+#endif
+
+
+/* Policies widely used by libarcstk */
+
+#ifndef __LIBARCSTK_POLICIES_HPP__
+#include <arcstk/policies.hpp>
+#endif
+
+
+/* Input: interfaces for toc metadata */
+
+#ifndef __LIBARCSTK_METADATA_HPP__
+#include <arcstk/metadata.hpp>
+#endif
+
+
+/* Input: interfaces for audio samples */
+
+#ifndef __LIBARCSTK_SAMPLES_HPP__
+#include <arcstk/samples.hpp>
+#endif
+
+
+/* Identifiers */
+
+#ifndef __LIBARCSTK_IDENTIFIER_HPP__
+#include <arcstk/identifier.hpp>
+#endif
+
+
+/* Checksum types */
+
+#ifndef __LIBARCSTK_CHECKSUM_HPP__
+#include <arcstk/checksum.hpp>
+#endif
+
+
+/* AccurateRip algorithms */
+
+#ifndef __LIBARCSTK_ALGORITHMS_HPP__
+#include <arcstk/algorithms.hpp>
+#endif
+
+
+/* Calculation interface */
+
+#ifndef __LIBARCSTK_CALCULATE_HPP__
+#include <arcstk/calculate.hpp>
+#endif
+
+
+/* DBAR parsing */
+
+#ifndef __LIBARCSTK_DBAR_HPP__
+#include <arcstk/dbar.hpp>
+#endif
+
+
+/* Checksum verification */
+
+#ifndef __LIBARCSTK_VERIFY_HPP__
+#include <arcstk/verify.hpp>
+#endif
+
+
+/* Version, Name and Release info */
+
+#ifndef __LIBARCSTK_VERSION_HPP__
+#include <arcstk/version.hpp>
+#endif
+
+#endif
+
