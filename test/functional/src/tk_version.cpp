1b51dac55f01879ccc495e6eea993dc747daa524
tristero <crf8472@mailbox.org>
2026-06-22 09:11:19 +0200

    Fix version test

--- test/functional/src/tk_version.cpp
+++ test/functional/src/tk_version.cpp
@@ -16,10 +16,10 @@ TEST_CASE ( "constants", "[version]" )
 	CHECK ( arcstk::v_1_0_0::LIBARCSTK_NAME == "libarcstk" );
 
 	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_MAJOR  == 0 );
-	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_MINOR  == 3 );
-	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_PATCH  == 1 );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_MINOR  == 9 );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_PATCH  == 0 );
 	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION_SUFFIX == "alpha.1" );
-	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION        == "0.3.1-alpha.1" );
+	CHECK ( arcstk::v_1_0_0::LIBARCSTK_VERSION        == "0.9.0-alpha.1" );
 }
 
 
@@ -37,7 +37,8 @@ TEST_CASE ( "functions", "[version]" )
 		CHECK (  api_version_is_at_least(0, 2, 0) );
 		CHECK (  api_version_is_at_least(0, 2, 1) );
 		CHECK (  api_version_is_at_least(0, 3, 0) );
-		CHECK (  api_version_is_at_least(0, 3, 1) ); // <= this version
+		CHECK (  api_version_is_at_least(0, 3, 1) );
+		CHECK (  api_version_is_at_least(0, 9, 0) ); // <= this version
 		CHECK ( !api_version_is_at_least(1, 0, 0) );
 		CHECK ( !api_version_is_at_least(1, 1, 0) );
 		CHECK ( !api_version_is_at_least(1, 1, 1) );
