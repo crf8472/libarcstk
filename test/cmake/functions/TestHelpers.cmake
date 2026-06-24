7f2b89fef44482eb38e984f144a761bdc85f3751
crf8472 <crf8472@mailbox.org>
2026-06-24 15:01:38 +0200

    Reorganize compiler flags for testing

--- test/cmake/functions/TestHelpers.cmake
+++ test/cmake/functions/TestHelpers.cmake
@@ -1,3 +1,47 @@
+## libarcstk: CMake build script for configuring and building tests
+## vim:fdm=marker
+
+## Make existing list of flags compliant for libarcstk tests
+function (_compile_flags_TEST OUT_VAR )
+
+	cmake_parse_arguments (CFLAGS "" "" "FLAGS" ${ARGN} )
+
+	set (_result "${CFLAGS_FLAGS}" )
+
+	## --- g++: Remove flags that pollute the output with false positives
+	if (CMAKE_COMPILER_IS_GNUCXX )
+
+		list (REMOVE_ITEM _result -Weffc++ )
+		list (REMOVE_ITEM _result -Wctor-dtor-privacy )
+		list (REMOVE_ITEM _result -Wuseless-cast )
+	endif()
+
+	set (${OUT_VAR} "${_result}" PARENT_SCOPE )
+endfunction ()
+
+##
+function (compile_flags OUT_VAR )
+
+	set (options TESTCASE )
+	set (multiValueArgs CURRENT )
+
+	cmake_parse_arguments (CFLAGS "${options}" "" "${multiValueArgs}" ${ARGN} )
+
+	##
+
+	if (CFLAGS_TESTCASE AND CFLAGS_CURRENT )
+
+		set (_actual_cxx_flags "${CFLAGS_CURRENT}" )
+
+		#_compile_flags_TEST (_actual_cxx_flags FLAGS "${_actual_cxx_flags}" )
+		_compile_flags_TEST (_actual_cxx_flags FLAGS ${_actual_cxx_flags} )
+
+		set (${OUT_VAR} ${_actual_cxx_flags} PARENT_SCOPE )
+		return ()
+	endif()
+
+endfunction ()
+
 ## Create a test executable from all .cpp files in a directory
 ## and register tests found with catch_discover_tests.
 ##
@@ -6,6 +50,8 @@
 ##       LABEL "unit"
 ##       TIMEOUT 10
 ##   )
+##
+## This produces a target called "unit_tests".
 function (add_test_suite CATEGORY )
 
 	set (options )
@@ -37,19 +83,24 @@ function (add_test_suite CATEGORY )
 		SKIP_RPATH             OFF
 	)
 
-	target_compile_options (${CATEGORY}_tests
-		PRIVATE ${TEST_CXX_FLAGS_WARNINGS}
-		PRIVATE ${LIBARCSTK_CXX_FLAGS_OPTIMIZE}
-	)
+	## Use flags from main target as a starting point
+	get_target_property (TEST_CXX_FLAGS ${PROJECT_NAME} COMPILE_OPTIONS )
+
+	## Generate flag set for tests from flag set of main target
+	compile_flags (TEST_CXX_FLAGS TESTCASE CURRENT ${TEST_CXX_FLAGS} )
+
+	target_compile_options (${CATEGORY}_tests PRIVATE ${TEST_CXX_FLAGS} )
+
+	get_target_property (${CATEGORY}_tests_CXX_FLAGS ${CATEGORY}_tests
+		COMPILE_OPTIONS )
+	message (STATUS "Compile flags for ${CATEGORY}_tests: "
+		"${${CATEGORY}_tests_CXX_FLAGS}" )
 
 	## Include paths
 	target_include_directories (${CATEGORY}_tests
-		PRIVATE "${LIBARCSTK_INCLUDE_SOURCE_DIR}"        ## public headers
-		PRIVATE "${LIBARCSTK_SOURCE_DIR}"                ## private headers
-		PRIVATE "${LIBARCSTK_GENSRC_BINARY_DIR}"         ## generated sources
-		PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/include"    ## test suite includes
-		#PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/fixtures"   ## test suite fixtures
-		#PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/../include" ## test helpers
+		PRIVATE "${LIBARCSTK_INCLUDE_SOURCE_DIR}" ## public headers
+		PRIVATE "${LIBARCSTK_SOURCE_DIR}"         ## private headers
+		PRIVATE "${LIBARCSTK_GENSRC_BINARY_DIR}"  ## generated sources
 	)
 
 	## Link libraries
@@ -57,11 +108,6 @@ function (add_test_suite CATEGORY )
 		PRIVATE Catch2::Catch2WithMain
 		PRIVATE ${PROJECT_NAME} ## libarcstk from build-tree
 	)
-
-	## RPATH handling (force to load from build tree)
-	#if (UNIX AND NOT APPLE )
-	#	target_link_options (${CATEGORY}_tests PRIVATE -Wl,--disable-new-dtags)
-	#endif()
 
 	## Set properties for all discovered tests
 
